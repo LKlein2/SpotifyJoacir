e928f99fa64497469ab0725f97f33db4fb11f696
Lucas <Lklein2@ucs.br>
2019-06-25 21:27:46 -0300

    Login funcionando

---
+++ app/src/main/java/com/example/spotifyjoacir/LoginActivity.java
@@ -0,0 +1,82 @@
+package com.example.spotifyjoacir;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "Spotify " + LoginActivity.class.getSimpleName();
+    private static final int REQUEST_CODE = 1337;
+    public static final String AUTH_TOKEN = "AUTH_TOKEN";
+    private static final String CLIENT_ID = "1d7edefde74f45beb393a66257a0ba49";
+    private static final String REDIRECT_URI = "http://localhost:8888/callback";
+    private String AUTH;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button mLoginButton = (Button)findViewById(R.id.buttonLogin);
+        mLoginButton.setOnClickListener(mListener);
+    }
+
+    private void openLoginWindow() {
+
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN,REDIRECT_URI);
+
+        builder.setScopes(new String[]{"streaming", "app-remote-control", });
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    View.OnClickListener mListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.buttonLogin:
+                    openLoginWindow();
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("AUTH_TOKEN", AUTH);
+        setResult(1,intent);
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, final int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE)        {
+            final AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, data);
+            switch (response.getType()) {
+                case TOKEN:
+                    AUTH = response.getAccessToken();
+                    onBackPressed();
+                    break;
+                case ERROR:
+                    AUTH = "ERROR";
+                    Log.e(TAG,"Auth error: " + response.getError());
+                    break;
+                default:
+                    AUTH = "ELSE";
+                    Log.d(TAG,"Auth result: " + response.getType());
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/spotifyjoacir/MainActivity.java
+++ app/src/main/java/com/example/spotifyjoacir/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.spotifyjoacir;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
@@ -11,53 +14,37 @@ import com.spotify.android.appremote.api.SpotifyAppRemote;
 import com.spotify.protocol.client.Subscription;
 import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String CLIENT_ID = "1d7edefde74f45beb393a66257a0ba49";
-    private static final String REDIRECT_URI = "http://localhost:8888/callback";
-    private SpotifyAppRemote mSpotifyAppRemote;
+    private static final String CLIENT_ID = "1d7edefde74f45beb393a66257a0ba49";     //This should be generate by spotify dashboard
+    private static final String REDIRECT_URI = "http://localhost:8888/callback";    //you should register it on spotify dashboard
+    private SpotifyAppRemote mSpotifyAppRemote;                                     //Besides that, register the SHA-1 key on spotify dashboard
+
+    private String AUTH_TOKEN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivityForResult(intent,1);
+
+        Toast.makeText(getApplicationContext(), AUTH_TOKEN, Toast.LENGTH_LONG).show();
     }
 
     @Override
-    protected void onStart() {
-
-        super.onStart();
-        ConnectionParams connectionParams =
-                new ConnectionParams.Builder(CLIENT_ID)
-                        .setRedirectUri(REDIRECT_URI)
-                        .showAuthView(true)
-                        .build();
-
-        SpotifyAppRemote.connect(this, connectionParams, new Connector.ConnectionListener() {
-            @Override
-            public void onConnected(SpotifyAppRemote spotifyAppRemote) {
-                mSpotifyAppRemote = spotifyAppRemote;
-                Log.d("Connected", "Yes");
-
-                connected();
-            }
-
-            @Override
-            public void onFailure(Throwable throwable) {
-                Log.e("MainActivity", throwable.getMessage(), throwable);
-            }
-        });
-    }
-
-    private void connected() {
-        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        switch (requestCode) {
+            case 1:
+                AUTH_TOKEN = intent.getExtras().getString("AUTH_TOKEN");
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, intent);
     }
 }
